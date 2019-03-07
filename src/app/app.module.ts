da4f3466ef8f422cf6cd37d534029749b4203706
Damian Wiśniewski <damian.wisniewski@asseco.pl>
2019-03-07 10:00:52 +0100

    Created database service and added component for adding Quicky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login/login.component';
 import { InterfaceComponent } from './main/interface/interface.component';
 import { QuickyComponent } from './main/quicky/quicky.component';
+import { AddQuickyComponent } from './main/add-quicky/add-quicky.component';
+import { DatabaseService } from './services/database.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     InterfaceComponent,
-    QuickyComponent
+    QuickyComponent,
+    AddQuickyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DatabaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/main/add-quicky/add-quicky.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-quicky',
+  templateUrl: './add-quicky.component.html',
+  styleUrls: ['./add-quicky.component.scss']
+})
+export class AddQuickyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
