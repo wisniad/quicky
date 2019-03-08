1d3293b39467dd0dc0e24d086d8c2114d1dbc7da
voizabela <izavo19@gmail.com>
2019-03-08 08:40:12 +0100

    add animations to quicky cards

--- src/app/main/quicky/quicky.component.ts
+++ src/app/main/quicky/quicky.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { trigger, keyframes, animate, transition } from '@angular/animations';
+import * as kf from './keyframes';
+
 @Component({
   selector: 'app-quicky',
   templateUrl: './quicky.component.html',
-  styleUrls: ['./quicky.component.scss']
+  styleUrls: ['./quicky.component.scss'],
+  animations: [
+    trigger('cardAnimator', [
+      transition('* => wobble', animate(1000, keyframes(kf.wobble))),
+      transition('* => swing', animate(1000, keyframes(kf.swing))),
+      transition('* => jello', animate(1000, keyframes(kf.jello))),
+      transition('* => zoomOutRight', animate(1000, keyframes(kf.zoomOutRight))),
+      transition('* => slideOutLeft', animate(1000, keyframes(kf.slideOutLeft))),
+      transition('* => rotateOutUpRight', animate(1000, keyframes(kf.rotateOutUpRight))),
+      transition('* => flipOutY', animate(1000, keyframes(kf.flipOutY))),
+    ])
+  ]
 })
 export class QuickyComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  animationState: string;
+
+  startAnimation(state) {
+    console.log(state)
+    if (!this.animationState) {
+      this.animationState = state;
+    }
+  }
+
+  resetAnimationState() {
+    this.animationState = '';
   }
 
 }
